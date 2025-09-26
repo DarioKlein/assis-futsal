7f38834b69442b2aa20d784cf50da31c0509068d
Dario Klein <dariokleinalves@gmail.com>
2025-09-26 02:50:56 -0300

    correção de erros do console, ajustes e adições no navbar desktop e mobile

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,8 +1,15 @@
-document.querySelector(".hamburguer-menu").addEventListener("click", () => {
-  const menuItems = document.querySelectorAll(".hamburguer-menu-item");
-  menuItems[0].classList.toggle("rotate-child-one");
-  menuItems[1].classList.toggle("hide-child-two");
-  menuItems[2].classList.toggle("rotate-child-three");
+document.querySelector('.hamburguer-menu').addEventListener('click', () => {
+  const menuItems = document.querySelectorAll('.hamburguer-menu-item')
+  menuItems[0].classList.toggle('rotate-child-one')
+  menuItems[1].classList.toggle('hide-child-two')
+  menuItems[2].classList.toggle('rotate-child-three')
 
-  document.querySelector(".menu").classList.toggle("visible-menu");
-});
+  document.querySelector('.menu').classList.toggle('visible-menu')
+  document.querySelector('.hamburguer-menu').classList.toggle('fix-close-button')
+
+  document.querySelectorAll('ul > li > a').forEach((e, i) => {
+    setTimeout(() => {
+      e.classList.toggle('animation-text')
+    }, i * 50)
+  })
+})
