c608f730a3ca8f8446fab96e63b529e38027eba7
Luis Felipe <2511550630@fema.edu.br>
2025-10-21 20:34:16 -0300

    Implementa lógica de foco no navbar

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,3 +1,25 @@
+const title = document.querySelector("title").innerText.toLowerCase();
+
+function setFocus() {
+  document.querySelectorAll(".navigation-section > ul > li").forEach((li) => {
+    const a = li.firstChild;
+
+    if (a.innerText.toLowerCase() === title) {
+      a.classList.add("navbar-focus");
+    }
+  });
+
+  document.querySelectorAll(".menu > ul > li").forEach((li) => {
+    const a = li.firstChild;
+
+    if (a.innerHTML.toLowerCase() === title) {
+      a.classList.add("navbar-focus");
+    }
+  });
+}
+
+setFocus();
+
 document.querySelector(".hamburguer-menu").addEventListener("click", () => {
   document.querySelector("#opacity").classList.toggle("opacity-active");
   const menuItems = document.querySelectorAll(".hamburguer-menu-item");
