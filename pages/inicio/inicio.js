a6140c5b438c420e2d9b7343ced60f9ef63aa76d
Luis Felipe <2511550630@fema.edu.br>
2025-11-07 16:51:34 -0300

    Implementa melhorias semânticas e correções

--- pages/inicio/inicio.js
+++ pages/inicio/inicio.js
@@ -1,56 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const loadComponents = async () => {
-    const elements = document.querySelectorAll('[data-include]')
+const palavra = "PARTICIPE";
+const sidePhrase = document.querySelector(".side-phrase");
 
-    for (const el of elements) {
-      const file = el.getAttribute('data-include')
+[...palavra].forEach((letra) => {
+  sidePhrase.innerHTML += `<span>${letra}</span>`;
+});
 
-      try {
-        const response = await fetch(file)
-        if (!response.ok) throw new Error(`Não foi possível carregar: ${file}`)
-
-        const html = await response.text()
-        el.innerHTML = html
-
-        const scriptPath = el.getAttribute('data-script')
-        if (scriptPath) {
-          const newScript = document.createElement('script')
-          newScript.src = scriptPath
-
-          document.body.appendChild(newScript)
-        }
-      } catch (error) {
-        console.error('Erro ao carregar componente:', error)
-        el.innerHTML = `<p style="color:red;">Erro ao carregar ${file}</p>`
-      }
-    }
-  }
-
-  loadComponents()
-})
-
-const palavra = 'PARTICIPE'
-const sidePhrase = document.querySelector('.side-phrase')
-
-;[...palavra].forEach(letra => {
-  sidePhrase.innerHTML += `<span>${letra}</span>`
-})
-
-const ctx = document.getElementById('myGraph')
-const data = [45, 35, 20]
-const porcentagens = data.map(n => {
-  return ((n / data.reduce((ac, val) => (ac += val))) * 100).toFixed(0)
-})
+const ctx = document.getElementById("myGraph");
+const data = [45, 35, 20];
+const porcentagens = data.map((n) => {
+  return ((n / data.reduce((ac, val) => (ac += val))) * 100).toFixed(0);
+});
 
 new Chart(ctx, {
-  type: 'pie',
+  type: "pie",
   data: {
-    labels: ['7 - 10 Anos', '11 - 14 Anos', '15 - 18 anos'],
+    labels: ["7 - 10 Anos", "11 - 14 Anos", "15 - 18 anos"],
     datasets: [
       {
         data: porcentagens,
-        label: '%',
-        backgroundColor: ['#f0d067', '#1092d7', '#1c4c83'],
+        label: "%",
+        backgroundColor: ["#f0d067", "#1092d7", "#1c4c83"],
         borderWidth: 0,
       },
     ],
@@ -60,38 +29,38 @@ new Chart(ctx, {
     plugins: {
       title: {
         display: true,
-        text: 'Faixa Etária dos Alunos',
+        text: "Faixa Etária dos Alunos",
         font: { size: 22 },
-        position: 'bottom',
-        color: '#1c4c83',
+        position: "bottom",
+        color: "#1c4c83",
       },
       legend: {
         labels: {
           padding: 20,
           font: { size: 10 },
-          pointStyle: 'circle',
+          pointStyle: "circle",
           usePointStyle: true,
-          color: '#000000',
+          color: "#000000",
         },
-        position: 'top',
+        position: "top",
       },
     },
   },
-})
+});
 
-const ctx1 = document.getElementById('myGraph1')
+const ctx1 = document.getElementById("myGraph1");
 
 new Chart(ctx1, {
-  type: 'line',
+  type: "line",
 
   data: {
-    labels: ['2021', '2022', '2023', '2024', '2025'],
+    labels: ["2021", "2022", "2023", "2024", "2025"],
     datasets: [
       {
-        label: '% de crescimento',
+        label: "% de crescimento",
         data: [18, 34, 39, 42, 50],
-        borderColor: '#1092d7',
-        backgroundColor: '#1092d733',
+        borderColor: "#1092d7",
+        backgroundColor: "#1092d733",
         tension: 0,
         // fill: true,
       },
@@ -103,16 +72,16 @@ new Chart(ctx1, {
     plugins: {
       title: {
         display: true,
-        text: 'Histórico de Crescimento',
+        text: "Histórico de Crescimento",
         font: { size: 26 },
-        color: '#1c4c83',
-        align: 'center',
+        color: "#1c4c83",
+        align: "center",
       },
       legend: {
         labels: {
-          color: '#1092d7',
+          color: "#1092d7",
         },
-        position: 'bottom',
+        position: "bottom",
       },
     },
     scales: {
@@ -121,12 +90,12 @@ new Chart(ctx1, {
 
         title: {
           display: true,
-          text: '(%)',
-          color: '#1c4c83',
+          text: "(%)",
+          color: "#1c4c83",
         },
         ticks: {
           stepSize: 10,
-          color: '#1c4c83',
+          color: "#1c4c83",
           font: { weight: 600, size: 14 },
         },
       },
@@ -135,16 +104,16 @@ new Chart(ctx1, {
           drawOnChartArea: false,
         },
         ticks: {
-          color: '#1c4c83',
+          color: "#1c4c83",
           font: { weight: 600, size: 14 },
         },
       },
     },
   },
-})
+});
 
-document.getElementById('button').addEventListener('click', () => {
-  document.getElementById('card-container-title').scrollIntoView({
-    behavior: 'smooth',
-  })
-})
+document.getElementById("button").addEventListener("click", () => {
+  document.getElementById("card-container-title").scrollIntoView({
+    behavior: "smooth",
+  });
+});
